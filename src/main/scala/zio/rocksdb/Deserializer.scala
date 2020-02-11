750e4fad9d46087a34c394584c77aaa2bb81a79b
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-11 17:31:27 +0000

    Collections

--- src/main/scala/zio/rocksdb/Deserializer.scala
+++ src/main/scala/zio/rocksdb/Deserializer.scala
@@ -2,19 +2,25 @@ package zio.rocksdb
 
 import java.{ lang => jlang }
 import java.nio.ByteBuffer
-import zio.{ UIO, ZIO }
+import zio.{ Chunk, Ref, Schedule, UIO, ZIO }
 
 trait Deserializer[-R, +A] { self =>
   def decode(bytes: Bytes): ZIO[R, DeserializeError, Result[A]]
 
   final def map[B](f: A => B): Deserializer[R, B] =
+    mapResult(_.map(f))
+
+  final def mapResult[B](f: Result[A] => Result[B]): Deserializer[R, B] =
     new Deserializer[R, B] {
-      def decode(bytes: Bytes): ZIO[R, DeserializeError, Result[B]] = self.decode(bytes).map(_.map(f))
+      def decode(bytes: Bytes): ZIO[R, DeserializeError, Result[B]] = self.decode(bytes).map(f)
     }
 
   final def mapM[R1 <: R, B](f: A => ZIO[R1, DeserializeError, B]): Deserializer[R1, B] =
+    mapMResult(_.mapM(f))
+
+  final def mapMResult[R1 <: R, B](f: Result[A] => ZIO[R1, DeserializeError, Result[B]]): Deserializer[R1, B] =
     new Deserializer[R1, B] {
-      def decode(bytes: Bytes): ZIO[R1, DeserializeError, Result[B]] = self.decode(bytes).flatMap(_.mapM(f))
+      def decode(bytes: Bytes): ZIO[R1, DeserializeError, Result[B]] = self.decode(bytes).flatMap(f)
     }
 
   final def flatMap[R1 <: R, B](f: A => Deserializer[R1, B]): Deserializer[R1, B] =
@@ -36,12 +42,38 @@ trait Deserializer[-R, +A] { self =>
 
 }
 
-object Deserializer extends PrimitiveDeserializers {
+object Deserializer extends CollectionDeserializers {
   def apply[R, A](f: Bytes => ZIO[R, DeserializeError, Result[A]]): Deserializer[R, A] =
     new Deserializer[R, A] {
       def decode(bytes: Bytes): ZIO[R, DeserializeError, Result[A]] = f(bytes)
     }
 
+}
+
+private[rocksdb] trait CollectionDeserializers extends PrimitiveDeserializers {
+  val bytes: Deserializer[Any, Bytes] =
+    Deserializer[Any, Bytes](bs => UIO.succeed(Result(bs, Chunk.empty)))
+
+  val byteArray: Deserializer[Any, Array[Byte]] =
+    bytes.map(_.toArray)
+
+  val chars: Deserializer[Any, Chunk[Char]] =
+    bytes.mapMResult(fromByteBuffer(char))
+
+  val doubles: Deserializer[Any, Chunk[Double]] =
+    bytes.mapMResult(fromByteBuffer(double))
+
+  def floats: Deserializer[Any, Chunk[Float]] =
+    bytes.mapMResult(fromByteBuffer(float))
+
+  def ints: Deserializer[Any, Chunk[Int]] =
+    bytes.mapMResult(fromByteBuffer(int))
+
+  val longs: Deserializer[Any, Chunk[Long]] =
+    bytes.mapMResult(fromByteBuffer(long))
+
+  val shorts: Deserializer[Any, Chunk[Short]] =
+    bytes.mapMResult(fromByteBuffer(short))
 }
 
 private[rocksdb] trait PrimitiveDeserializers extends DeserializerUtilityFunctions {
@@ -88,4 +120,23 @@ private[rocksdb] trait DeserializerUtilityFunctions {
           }
       }
     }
+
+  def fromByteBuffer[R, A](
+    deser: Deserializer[R, A]
+  )(bytes: Result[Bytes]): ZIO[R, DeserializeError, Result[Chunk[A]]] =
+    for {
+      bs <- Ref.make(bytes.value)
+      as <- Ref.make[Chunk[A]](Chunk.empty)
+      _ <- bs.get
+            .flatMap(deser.decode)
+            .flatMap {
+              case Result(a, bytes) => bs.set(bytes) *> as.update(_ + a)
+            }
+            .repeatOrElseEither[R, Any, DeserializeError, Unit](Schedule.forever, {
+              case (DeserializeError.TooShort(_, _), _) => ZIO.unit
+              case (error, _)                           => ZIO.fail(error)
+            })
+      result <- as.get.zipWith(bs.get)(Result.apply)
+    } yield result
+
 }
