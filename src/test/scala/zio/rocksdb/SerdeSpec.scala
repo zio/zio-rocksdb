93b324b7db7dcb6e49661df46f031a5b4e0b0cea
Regis Kuckaertz <regis.kuckaertz@gmail.com>
2020-02-24 16:11:25 +0000

    Add tests

---
+++ src/test/scala/zio/rocksdb/SerdeSpec.scala
@@ -0,0 +1,61 @@
+package zio.rocksdb
+
+import zio.Chunk
+import zio.test._
+import zio.test.Assertion._
+import SerdeUtils._
+
+object SerdeSpec
+    extends DefaultRunnableSpec(
+      suite("SerdeSpec")(
+        suite("primitive types")(
+          roundtrip("boolean", Gen.boolean, Serializer.boolean, Deserializer.boolean),
+          roundtrip("byte", Gen.anyByte, Serializer.byte, Deserializer.byte),
+          roundtrip("char", Gen.anyChar, Serializer.char, Deserializer.char),
+          roundtrip("double", Gen.double(Double.MinValue, Double.MaxValue), Serializer.double, Deserializer.double),
+          roundtrip("float", Gen.anyFloat, Serializer.float, Deserializer.float),
+          roundtrip("int", Gen.anyInt, Serializer.int, Deserializer.int),
+          roundtrip("long", Gen.anyLong, Serializer.long, Deserializer.long),
+          roundtrip("short", Gen.anyShort, Serializer.short, Deserializer.short)
+        ),
+        suite("tuples")(
+          roundtrip(
+            "tuple2",
+            Gen.anyInt zip Gen.anyInt,
+            Serializer.tuple2(Serializer.int, Serializer.int),
+            Deserializer.tuple2(Deserializer.int, Deserializer.int)
+          ),
+          roundtrip(
+            "tuple3",
+            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case ((a, b), c) => (a, b, c) },
+            Serializer.tuple3(Serializer.int, Serializer.int, Serializer.int),
+            Deserializer.tuple3(Deserializer.int, Deserializer.int, Deserializer.int)
+          ),
+          roundtrip(
+            "tuple4",
+            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case (((a, b), c), d) => (a, b, c, d) },
+            Serializer.tuple4(Serializer.int, Serializer.int, Serializer.int, Serializer.int),
+            Deserializer.tuple4(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
+          ),
+          roundtrip(
+            "tuple5",
+            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map {
+              case ((((a, b), c), d), e) => (a, b, c, d, e)
+            },
+            Serializer.tuple5(Serializer.int, Serializer.int, Serializer.int, Serializer.int, Serializer.int),
+            Deserializer
+              .tuple5(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
+          )
+        ),
+        suite("collections")(
+          roundtrip("lists", Gen.listOf(Gen.anyInt), Serializer.ints[List], Deserializer.list(Deserializer.ints))
+        )
+      )
+    )
+
+object SerdeUtils {
+  def roundtrip[R, A](label: String, gen: Gen[R, A], ser: Serializer[R, A], deser: Deserializer[R, A]) =
+    testM(label)(checkM(gen) { x =>
+      assertM(ser(x) >>= deser.decode, equalTo(Result(x, Chunk.empty)))
+    })
+}
