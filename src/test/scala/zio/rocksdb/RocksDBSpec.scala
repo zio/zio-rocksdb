9d56dedf8e4073136bfc621323894bdf690bb936
Dejan Mijic <dmijic@acm.org>
2020-03-04 01:32:15 +0100

    Factor-out common layer

--- src/test/scala/zio/rocksdb/RocksDBSpec.scala
+++ src/test/scala/zio/rocksdb/RocksDBSpec.scala
@@ -13,48 +13,43 @@ import zio.test.Assertion._
 import scala.jdk.CollectionConverters._
 
 object RocksDBSpec extends DefaultRunnableSpec {
-  override def spec = suite("RocksDB")(
-    testM("get/put") {
-      val key   = "key".getBytes(UTF_8)
-      val value = "value".getBytes(UTF_8)
+  override def spec = {
+    val rocksSuite = suite("RocksDB")(
+      testM("get/put") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
 
-      val test =
         for {
           _      <- rocksdb.put(key, value)
           result <- rocksdb.get(key)
         } yield assert(result)(isSome(equalTo(value)))
+      },
+      testM("delete") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
 
-      test.provideLayer(database)
-    },
-    testM("delete") {
-      val key   = "key".getBytes(UTF_8)
-      val value = "value".getBytes(UTF_8)
-
-      val test =
         for {
           _      <- rocksdb.put(key, value)
           before <- rocksdb.get(key)
           _      <- rocksdb.delete(key)
           after  <- rocksdb.get(key)
         } yield assert(before)(isSome(equalTo(value))) && assert(after)(isNone)
+      },
+      testM("newIterator") {
+        val data = (1 to 10).map(i => (s"key$i", s"value$i")).toList
 
-      test.provideLayer(database)
-    },
-    testM("newIterator") {
-      val data = (1 to 10).map(i => (s"key$i", s"value$i")).toList
-
-      val test =
         for {
           _          <- RIO.foreach(data) { case (k, v) => rocksdb.put(k.getBytes(UTF_8), v.getBytes(UTF_8)) }
           results    <- rocksdb.newIterator.runCollect
           resultsStr = results.map { case (k, v) => new String(k, UTF_8) -> new String(v, UTF_8) }
         } yield assert(resultsStr)(hasSameElements(data))
+      }
+    )
 
-      test.provideLayer(database)
-    }
-  )
+    rocksSuite.provideCustomLayerShared(database)
+  }
 
-  private def database: ZLayer.NoDeps[Throwable, RocksDB] =
+  private val database =
     ZLayer.fromManaged {
       Task(Files.createTempDirectory("zio-rocksdb")).toManaged { path =>
         UIO {
@@ -71,5 +66,5 @@ object RocksDBSpec extends DefaultRunnableSpec {
         val opts = new Options().setCreateIfMissing(true)
         Live.open(opts, dir.toAbsolutePath.toString)
       }
-    }
+    }.mapError(TestFailure.die)
 }
