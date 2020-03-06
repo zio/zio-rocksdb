4ce75ecfd05f78ecc9b0f7d79b51426c108e7433
Dejan Mijić <dmijic@acm.org>
2020-03-06 08:50:22 +0100

    Upgrade ZIO to 1.0.0-RC18 (#9)
    * Bump ZIO to RC18
    
    * Rename fromPull to repeatEffectOption
    
    * Update specs format
    
    * Migrate environment to ZLayer
    
    * Pull-out Live to separate file
    
    * Factor-out common layer
    
    * Use foreach_ instead of foreach

--- src/test/scala/zio/rocksdb/RocksDBSpec.scala
+++ src/test/scala/zio/rocksdb/RocksDBSpec.scala
@@ -2,78 +2,69 @@ package zio.rocksdb
 
 import java.io.File
 import java.nio.charset.StandardCharsets.UTF_8
-import java.nio.file.{ Files, Path }
+import java.nio.file.Files
 
 import org.rocksdb.Options
-import zio.{ Managed, RIO, Task, UIO }
+import zio.{ RIO, Task, UIO, ZLayer }
 import zio.rocksdb
 import zio.test._
 import zio.test.Assertion._
 
 import scala.jdk.CollectionConverters._
 
-object RocksDBSpec
-    extends DefaultRunnableSpec(
-      suite("RocksDB")(
-        testM("get/put") {
-          val key   = "key".getBytes(UTF_8)
-          val value = "value".getBytes(UTF_8)
+object RocksDBSpec extends DefaultRunnableSpec {
+  override def spec = {
+    val rocksSuite = suite("RocksDB")(
+      testM("get/put") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
 
-          val test =
-            for {
-              _      <- rocksdb.put(key, value)
-              result <- rocksdb.get(key)
-            } yield assert(result, isSome(equalTo(value)))
+        for {
+          _      <- rocksdb.put(key, value)
+          result <- rocksdb.get(key)
+        } yield assert(result)(isSome(equalTo(value)))
+      },
+      testM("delete") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
 
-          test.provideManaged(Utils.tempDB)
-        },
-        testM("delete") {
-          val key   = "key".getBytes(UTF_8)
-          val value = "value".getBytes(UTF_8)
+        for {
+          _      <- rocksdb.put(key, value)
+          before <- rocksdb.get(key)
+          _      <- rocksdb.delete(key)
+          after  <- rocksdb.get(key)
+        } yield assert(before)(isSome(equalTo(value))) && assert(after)(isNone)
+      },
+      testM("newIterator") {
+        val data = (1 to 10).map(i => (s"key$i", s"value$i")).toList
 
-          val test =
-            for {
-              _      <- rocksdb.put(key, value)
-              before <- rocksdb.get(key)
-              _      <- rocksdb.delete(key)
-              after  <- rocksdb.get(key)
-            } yield assert(before, isSome(equalTo(value))) && assert(after, isNone)
-
-          test.provideManaged(Utils.tempDB)
-        },
-        testM("newIterator") {
-          val data = (1 to 10).map(i => (s"key$i", s"value$i")).toList
-
-          val test =
-            for {
-              _          <- RIO.foreach(data) { case (k, v) => rocksdb.put(k.getBytes(UTF_8), v.getBytes(UTF_8)) }
-              results    <- rocksdb.newIterator.runCollect
-              resultsStr = results.map { case (k, v) => new String(k, UTF_8) -> new String(v, UTF_8) }
-            } yield assert(resultsStr, hasSameElements(data))
-
-          test.provideManaged(Utils.tempDB)
-        }
-      )
+        for {
+          _          <- RIO.foreach_(data) { case (k, v) => rocksdb.put(k.getBytes(UTF_8), v.getBytes(UTF_8)) }
+          results    <- rocksdb.newIterator.runCollect
+          resultsStr = results.map { case (k, v) => new String(k, UTF_8) -> new String(v, UTF_8) }
+        } yield assert(resultsStr)(hasSameElements(data))
+      }
     )
 
-object Utils {
-  def tempDB: Managed[Throwable, RocksDB] =
-    tempDir.flatMap { dir =>
-      val opts = new Options().setCreateIfMissing(true)
-      RocksDB.Live.open(opts, dir.toAbsolutePath.toString)
-    }
+    rocksSuite.provideCustomLayerShared(database)
+  }
 
-  private def tempDir: Managed[Throwable, Path] =
-    Task(Files.createTempDirectory("zio-rocksdb")).toManaged { path =>
-      UIO {
-        Files
-          .walk(path)
-          .iterator()
-          .asScala
-          .toList
-          .map(_.toFile)
-          .sorted((o1: File, o2: File) => -o1.compareTo(o2))
-          .foreach(_.delete)
+  private val database =
+    ZLayer.fromManaged {
+      Task(Files.createTempDirectory("zio-rocksdb")).toManaged { path =>
+        UIO {
+          Files
+            .walk(path)
+            .iterator()
+            .asScala
+            .toList
+            .map(_.toFile)
+            .sorted((o1: File, o2: File) => -o1.compareTo(o2))
+            .foreach(_.delete)
+        }
+      }.flatMap { dir =>
+        val opts = new Options().setCreateIfMissing(true)
+        Live.open(opts, dir.toAbsolutePath.toString)
       }
-    }
+    }.mapError(TestFailure.die)
 }
--- src/test/scala/zio/rocksdb/SerdeSpec.scala
+++ src/test/scala/zio/rocksdb/SerdeSpec.scala
@@ -3,70 +3,66 @@ package zio.rocksdb
 import zio.Chunk
 import zio.test._
 import zio.test.Assertion._
-import SerdeUtils._
 
-object SerdeSpec
-    extends DefaultRunnableSpec(
-      suite("SerdeSpec")(
-        suite("primitive types")(
-          roundtrip("boolean", Gen.boolean, Serializer.boolean, Deserializer.boolean),
-          roundtrip("byte", Gen.anyByte, Serializer.byte, Deserializer.byte),
-          roundtrip("char", Gen.anyChar, Serializer.char, Deserializer.char),
-          roundtrip("double", Gen.double(Double.MinValue, Double.MaxValue), Serializer.double, Deserializer.double),
-          roundtrip("float", Gen.anyFloat, Serializer.float, Deserializer.float),
-          roundtrip("int", Gen.anyInt, Serializer.int, Deserializer.int),
-          roundtrip("long", Gen.anyLong, Serializer.long, Deserializer.long),
-          roundtrip("short", Gen.anyShort, Serializer.short, Deserializer.short)
-        ),
-        suite("tuples")(
-          roundtrip(
-            "tuple2",
-            Gen.anyInt zip Gen.anyInt,
-            Serializer.tuple2(Serializer.int, Serializer.int),
-            Deserializer.tuple2(Deserializer.int, Deserializer.int)
-          ),
-          roundtrip(
-            "tuple3",
-            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case ((a, b), c) => (a, b, c) },
-            Serializer.tuple3(Serializer.int, Serializer.int, Serializer.int),
-            Deserializer.tuple3(Deserializer.int, Deserializer.int, Deserializer.int)
-          ),
-          roundtrip(
-            "tuple4",
-            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case (((a, b), c), d) => (a, b, c, d) },
-            Serializer.tuple4(Serializer.int, Serializer.int, Serializer.int, Serializer.int),
-            Deserializer.tuple4(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
-          ),
-          roundtrip(
-            "tuple5",
-            (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map {
-              case ((((a, b), c), d), e) => (a, b, c, d, e)
-            },
-            Serializer.tuple5(Serializer.int, Serializer.int, Serializer.int, Serializer.int, Serializer.int),
-            Deserializer
-              .tuple5(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
-          )
-        ),
-        suite("collections")(
-          roundtrip(
-            "lists",
-            Gen.listOf(Gen.anyInt),
-            Serializer.ints[List],
-            Deserializer.list(Deserializer.chunk(Deserializer.int))
-          ),
-          roundtrip(
-            "maps",
-            Gen.listOf(Gen.anyInt zip Gen.anyShort).map(_.toMap),
-            Serializer.map(Serializer.int, Serializer.short),
-            Deserializer.map(Deserializer.chunk(Deserializer.int zip Deserializer.short))
-          )
-        )
+object SerdeSpec extends DefaultRunnableSpec {
+  override def spec = suite("SerdeSpec")(
+    suite("primitive types")(
+      roundtrip("boolean", Gen.boolean, Serializer.boolean, Deserializer.boolean),
+      roundtrip("byte", Gen.anyByte, Serializer.byte, Deserializer.byte),
+      roundtrip("char", Gen.anyChar, Serializer.char, Deserializer.char),
+      roundtrip("double", Gen.double(Double.MinValue, Double.MaxValue), Serializer.double, Deserializer.double),
+      roundtrip("float", Gen.anyFloat, Serializer.float, Deserializer.float),
+      roundtrip("int", Gen.anyInt, Serializer.int, Deserializer.int),
+      roundtrip("long", Gen.anyLong, Serializer.long, Deserializer.long),
+      roundtrip("short", Gen.anyShort, Serializer.short, Deserializer.short)
+    ),
+    suite("tuples")(
+      roundtrip(
+        "tuple2",
+        Gen.anyInt zip Gen.anyInt,
+        Serializer.tuple2(Serializer.int, Serializer.int),
+        Deserializer.tuple2(Deserializer.int, Deserializer.int)
+      ),
+      roundtrip(
+        "tuple3",
+        (Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case ((a, b), c) => (a, b, c) },
+        Serializer.tuple3(Serializer.int, Serializer.int, Serializer.int),
+        Deserializer.tuple3(Deserializer.int, Deserializer.int, Deserializer.int)
+      ),
+      roundtrip(
+        "tuple4",
+        (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map { case (((a, b), c), d) => (a, b, c, d) },
+        Serializer.tuple4(Serializer.int, Serializer.int, Serializer.int, Serializer.int),
+        Deserializer.tuple4(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
+      ),
+      roundtrip(
+        "tuple5",
+        (Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt zip Gen.anyInt).map {
+          case ((((a, b), c), d), e) => (a, b, c, d, e)
+        },
+        Serializer.tuple5(Serializer.int, Serializer.int, Serializer.int, Serializer.int, Serializer.int),
+        Deserializer
+          .tuple5(Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int, Deserializer.int)
+      )
+    ),
+    suite("collections")(
+      roundtrip(
+        "lists",
+        Gen.listOf(Gen.anyInt),
+        Serializer.ints[List],
+        Deserializer.list(Deserializer.chunk(Deserializer.int))
+      ),
+      roundtrip(
+        "maps",
+        Gen.listOf(Gen.anyInt zip Gen.anyShort).map(_.toMap),
+        Serializer.map(Serializer.int, Serializer.short),
+        Deserializer.map(Deserializer.chunk(Deserializer.int zip Deserializer.short))
       )
     )
+  )
 
-object SerdeUtils {
-  def roundtrip[R, A](label: String, gen: Gen[R, A], ser: Serializer[R, A], deser: Deserializer[R, A]) =
+  private def roundtrip[R, A](label: String, gen: Gen[R, A], ser: Serializer[R, A], deser: Deserializer[R, A]) =
     testM(label)(checkM(gen) { x =>
-      assertM(ser(x) >>= deser.decode, equalTo(Result(x, Chunk.empty)))
+      assertM(ser(x) >>= deser.decode)(equalTo(Result(x, Chunk.empty)))
     })
 }
