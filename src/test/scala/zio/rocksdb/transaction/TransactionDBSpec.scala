9fb922e6fe284b508e72e84130e42db1dd299191
Tushar Mathur <tusharmath@gmail.com>
2020-04-11 19:47:36 +0530

    test(transaction): add TransactionDBSpec

---
+++ src/test/scala/zio/rocksdb/transaction/TransactionDBSpec.scala
@@ -0,0 +1,68 @@
+package zio.rocksdb.transaction
+
+import java.nio.charset.StandardCharsets.UTF_8
+
+import internal.ManagedPath
+import org.{ rocksdb => jrocks }
+import zio.duration._
+import zio.test.Assertion.{ equalTo, isSome }
+import zio.test.TestAspect._
+import zio.test._
+import zio.{ console, ZIO, ZLayer }
+
+import scala.language.postfixOps
+
+object TransactionDBSpec extends DefaultRunnableSpec {
+
+  override def spec =
+    suite("TransactionDBSpec")(
+      testM("get/put") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
+        for {
+          result <- RocksDB.transaction(for {
+                     _      <- put(key, value)
+                     result <- get(key)
+                   } yield result)
+        } yield assert(result)(isSome(equalTo(value)))
+      },
+      testM("get/put with Console") {
+        val key   = "key".getBytes(UTF_8)
+        val value = "value".getBytes(UTF_8)
+
+        for {
+          result <- RocksDB.transaction(for {
+                     _      <- put(key, value)
+                     result <- get(key)
+                     _      <- console.putStrLn(result.toString)
+                   } yield result)
+        } yield assert(result)(isSome(equalTo(value)))
+      },
+      testM("concurrent updates to the same key") {
+        val count    = 10
+        val key      = "COUNT".getBytes(UTF_8)
+        val expected = isSome(equalTo(count))
+
+        for {
+          _ <- RocksDB.transaction(put(key, 0.toString.getBytes(UTF_8)))
+          _ <- concurrent(count) {
+                RocksDB.transaction(getForUpdate(key, exclusive = true) >>= { iCount =>
+                  put(key, iCount.map(bytesToInt).map(_ + 1).getOrElse(-1).toString.getBytes(UTF_8))
+
+                })
+              }
+          actual <- RocksDB.transaction(get(key))
+        } yield assert(actual.map(bytesToInt))(expected)
+      }
+    ).provideCustomLayerShared(dbLayer) @@ timeout(1 second)
+
+  private def bytesToInt(bytes: Array[Byte]): Int                                = new String(bytes, UTF_8).toInt
+  private def concurrent[R, E, A](n: Int)(zio: ZIO[R, E, A]): ZIO[R, E, List[A]] = ZIO.foreachPar(0 until n)(_ => zio)
+  private def dbLayer: ZLayer[Any, TestFailure[Nothing], TransactionDB] =
+    ZLayer
+      .fromManaged(ManagedPath() >>= { dir =>
+        ZTransactionDB.open(new jrocks.Options().setCreateIfMissing(true), dir.toAbsolutePath.toString)
+      })
+      .mapError(TestFailure.die)
+
+}
