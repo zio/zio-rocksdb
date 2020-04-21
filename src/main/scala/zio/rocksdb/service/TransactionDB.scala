d744a41fc4014d59fa129cbea5c4ed0b7d3de279
Tushar Mathur <tusharmath@gmail.com>
2020-04-21 09:53:22 +0530

    doc: add scala doc

--- src/main/scala/zio/rocksdb/service/TransactionDB.scala
+++ src/main/scala/zio/rocksdb/service/TransactionDB.scala
@@ -5,19 +5,41 @@ import zio.{ Has, ZIO, ZManaged }
 import zio.rocksdb.Atomically
 
 trait TransactionDB extends RocksDB {
+
+  /**
+   * Creates a managed instance of `service.Transaction` using the provided `WriteOptions`.
+   */
   def beginTransaction(writeOptions: jrocks.WriteOptions): ZManaged[Any, Nothing, Transaction]
+
+  /**
+   * Creates a managed instance of `service.Transaction`.
+   */
   def beginTransaction: ZManaged[Any, Nothing, Transaction] = beginTransaction(new jrocks.WriteOptions())
+
+  /**
+   * Executes the provided zio program in a single transaction.
+   */
   def atomically[R <: Has[_], E >: Throwable, A](writeOptions: jrocks.WriteOptions)(
     zio: ZIO[Has[Transaction] with R, E, A]
   )(implicit A: Atomically.TransactionWithSomething): ZIO[R, E, A]
+
+  /**
+   * Executes the provided zio program in a single transaction.
+   */
   def atomically[R <: Has[_], E >: Throwable, A](zio: ZIO[Has[Transaction] with R, E, A])(
     implicit A: Atomically.TransactionWithSomething
   ): ZIO[R, E, A] = atomically[R, E, A](new jrocks.WriteOptions())(zio)
 
+  /**
+   * Executes the provided zio program in a single transaction.
+   */
   def atomically[E >: Throwable, A](writeOptions: jrocks.WriteOptions)(zio: ZIO[Has[Transaction], E, A])(
     implicit A: Atomically.TransactionOnly
   ): ZIO[Any, E, A]
 
+  /**
+   * Executes the provided zio program in a single transaction.
+   */
   def atomically[E >: Throwable, A](zio: ZIO[Has[Transaction], E, A])(
     implicit A: Atomically.TransactionOnly
   ): ZIO[Any, E, A] = atomically[E, A](new jrocks.WriteOptions())(zio)
