5b46cf86e61eadc098e69dedf1264d6f9c0d6db1
ashprakasan <prakasanaiswarya@gmail.com>
2021-12-22 12:21:08 +0530

    Add more functions to Transaction

--- src/main/scala/zio/rocksdb/Transaction.scala
+++ src/main/scala/zio/rocksdb/Transaction.scala
@@ -1,5 +1,6 @@
 package zio.rocksdb
-import org.rocksdb.ReadOptions
+
+import org.rocksdb.{ ColumnFamilyHandle, ReadOptions }
 import org.{ rocksdb => jrocks }
 import zio._
 
@@ -8,20 +9,53 @@ object Transaction {
   private final class Live private (semaphore: Semaphore, transaction: jrocks.Transaction) extends service.Transaction {
 
     def taskWithPermit[A](task: => A): Task[A] = semaphore.withPermit(Task(task))
-    def uioWithPermit[A](task: => A): UIO[A]   = semaphore.withPermit(UIO(task))
+
+    def uioWithPermit[A](task: => A): UIO[A] = semaphore.withPermit(UIO(task))
+
     override def get(readOptions: jrocks.ReadOptions, key: Array[Byte]): Task[Option[Array[Byte]]] = taskWithPermit {
       Option(transaction.get(readOptions, key))
     }
+
     override def getForUpdate(
       readOptions: ReadOptions,
       key: Array[Byte],
       exclusive: Boolean
-    ): Task[Option[Array[Byte]]]                                       = taskWithPermit { Option(transaction.getForUpdate(readOptions, key, exclusive)) }
-    override def put(key: Array[Byte], value: Array[Byte]): Task[Unit] = taskWithPermit { transaction.put(key, value) }
-    override def delete(key: Array[Byte]): Task[Unit]                  = taskWithPermit { transaction.delete(key) }
-    override def commit: Task[Unit]                                    = taskWithPermit { transaction.commit() }
-    override def close: UIO[Unit]                                      = uioWithPermit { transaction.close() }
-    override def rollback: Task[Unit]                                  = taskWithPermit { transaction.rollback() }
+    ): Task[Option[Array[Byte]]] = taskWithPermit {
+      Option(transaction.getForUpdate(readOptions, key, exclusive))
+    }
+
+    override def put(key: Array[Byte], value: Array[Byte]): Task[Unit] = taskWithPermit {
+      transaction.put(key, value)
+    }
+
+    override def delete(key: Array[Byte]): Task[Unit] = taskWithPermit {
+      transaction.delete(key)
+    }
+
+    override def commit: Task[Unit] = taskWithPermit {
+      transaction.commit()
+    }
+
+    override def close: UIO[Unit] = uioWithPermit {
+      transaction.close()
+    }
+
+    override def rollback: Task[Unit] = taskWithPermit {
+      transaction.rollback()
+    }
+
+    override def getForUpdate(
+      readOptions: ReadOptions,
+      cf: ColumnFamilyHandle,
+      key: Array[Byte],
+      exclusive: Boolean
+    ): Task[Array[Byte]] = taskWithPermit {
+      transaction.getForUpdate(readOptions, cf, key, exclusive)
+    }
+
+    override def put(cf: ColumnFamilyHandle, key: Array[Byte], value: Array[Byte]): Task[Unit] = taskWithPermit {
+      transaction.put(cf, key, value)
+    }
   }
 
   object Live {
@@ -43,24 +77,32 @@ object Transaction {
 
   def get(readOptions: jrocks.ReadOptions, key: Array[Byte]): RIO[Transaction, Option[Array[Byte]]] =
     RIO.accessM(_.get.get(readOptions, key))
+
   def get(key: Array[Byte]): RIO[Transaction, Option[Array[Byte]]] =
     RIO.accessM(_.get.get(key))
+
   def getForUpdate(
     readOptions: jrocks.ReadOptions,
     key: Array[Byte],
     exclusive: Boolean
   ): RIO[Transaction, Option[Array[Byte]]] =
     RIO.accessM(_.get.getForUpdate(readOptions, key, exclusive))
+
   def getForUpdate(key: Array[Byte], exclusive: Boolean): RIO[Transaction, Option[Array[Byte]]] =
     RIO.accessM(_.get.getForUpdate(key, exclusive))
+
   def put(key: Array[Byte], value: Array[Byte]): RIO[Transaction, Unit] =
     RIO.accessM(_.get.put(key, value))
+
   def delete(key: Array[Byte]): RIO[Transaction, Unit] =
     RIO.accessM(_.get.delete(key))
+
   def commit: RIO[Transaction, Unit] =
     RIO.accessM(_.get.commit)
+
   def close: URIO[Transaction, Unit] =
     RIO.accessM(_.get.close)
+
   def rollback: RIO[Transaction, Unit] =
     RIO.accessM(_.get.rollback)
 
